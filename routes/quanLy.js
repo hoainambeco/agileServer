fef10ac67aff838041c174f40cf0815a09521046
Nguyễn Hoài Nam <78219804+hoainambeco@users.noreply.github.com>
2022-06-19 11:45:36 +0700

    sửa lại api thống kê

--- routes/quanLy.js
+++ routes/quanLy.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var quanLyController = require('../controlers/quanLy.controlers');
 router.get('/', quanLyController.thongke);
-router.post('month', quanLyController.thongke);
+router.post('/month', quanLyController.thongke);
 router.get('/All', quanLyController.thongKeTong);
 router.post('/All', quanLyController.thongKeTong);
 module.exports = router;
